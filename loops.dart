53820ff0c58f2fba56f075817da0e7a7dc30840b
Amit Srivastava <amit.srivastava4all@gmail.com>
2024-09-29 16:06:31 +0530

    Add files via upload

---
+++ loops.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Loop, for, while , do while
+  // for (int i = 1; i <= 10; i++) {
+  //   print("Amit");
+  // }
+  // int i = 1;
+  // while (i <= 10) {
+  //   print("Amit $i");
+  //   if (i <= 5) {
+  //     i++; // i = i + 1
+  //   } else {
+  //     i = i + 2;
+  //   }
+  // }
+  int i = 1;
+  do {
+    print("Amit $i");
+    i++;
+  } while (i >= 10);
+}
--- switchdemo.dart
+++ switchdemo.dart
@@ -1,30 +1,66 @@
 import 'dart:io';
-//import 'dart:core';
 
 void main() {
-  const int BURGER = 1;
-  print("Choose Your Item to Buy");
+  // switch expression
+
+  const BURGER = 1;
+  const DRINKS = 2;
+  const DESSERT = 3;
+  const PIZZA = 4;
+  const BIGBURGER = 5;
   print("1. Burger");
-  print("4. Cheese Burger");
-  print("2. Soft Drink");
-  print("3. Desserts");
-  print("Enter Your Choice ");
+  print("2. Drinks");
+  print("3. Dessert");
+  print("Enter the Choice");
   int choice = int.parse(stdin.readLineSync()!);
-  switch (choice) {
-    case BURGER:
-    case 4:
-      print("Burger Price is Rs 200");
-    case 2:
-      print("Soft Drink Price is Rs 100");
+  // var result = switch (choice) {
+  //   BURGER || BIGBURGER => "Burger Price is Rs 150",
+  //   >= 10 && <= 15 => "Cold Drinks",
+  //   _ => "Wrong Choice"
+  // };
+  // print(result);
+  // var result = "";
+  // switch (choice) {
+  //   case BURGER:
+  //     result = "Price is Rs 200";
+  //   case PIZZA:
+  //     result = "Price is Rs 300";
+  // }
+  // print(result);
+  // if(){
+  //   x = 10;
+  // }
+  // else{
+  //   x = 20;
+  // }
+  //var x = 10>20?10:20;
+  print(switch (choice) {
+    BURGER || BIGBURGER => "Price is rs 200",
+    PIZZA => "Price is Rs 300",
+    >= 10 && <= 15 => "Cold Drinks",
+    _ => "Wrong Choice"
+  });
+  print(10 + 20);
+  print(switch (choice) {
+    BURGER || BIGBURGER => "Burger Price is Rs 150",
+    >= 10 && <= 15 => "Cold Drinks",
+    _ => "Wrong Choice"
+  });
+  // switch case
+  // switch (choice) {
+  //   case BURGER:
+  //   case BIGBURGER:
+  //     print("Burger Rs 150 and U can also like Pizza");
+  //     continue food;
+  //   food:
+  //   case PIZZA:
+  //     print("Pizza Rs 200");
+  //   case DRINKS:
+  //     print("Rs 100");
+  //   case DESSERT:
+  //     print("Rs 400");
 
-    case 3:
-      print("Dessert Price is Rs 300");
-      continue icecream;
-
-    icecream:
-    case 5:
-      print("Dessert Price is Rs 400");
-    default:
-      print("OOPS Wrong Choice");
-  }
+  //   default:
+  //     print("Wrong Choice...");
+  // }
 }
